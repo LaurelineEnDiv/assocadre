7a57b05e29a439dfbf2b8c274e33ae233f37b180
Laureline Auger <laureline.auger@gmail.com>
2025-11-21 14:29:12 +0100

    page Bibliographie

--- functions.php
+++ functions.php
@@ -54,3 +54,58 @@ function liste_documents_shortcode() {
     return ob_get_clean();
 }
 add_shortcode('liste_documents', 'liste_documents_shortcode');
+
+// === Shortcode Bibliographie === //
+// ===== Shortcode Bibliographie ===== //
+function shortcode_bibliographie() {
+
+    $args = array(
+        'post_type'      => 'bibliographie',
+        'posts_per_page' => -1,
+        'orderby'        => 'title',
+        'order'          => 'ASC'
+    );
+
+    $livres = new WP_Query($args);
+
+    ob_start();
+
+    if ( $livres->have_posts() ) : ?>
+
+        <div class="bibliographie-grid">
+            <?php while ( $livres->have_posts() ) : $livres->the_post(); ?>
+
+                <div class="livre-item">
+
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <div class="livre-image-wrapper">
+                            <?php the_post_thumbnail('medium'); ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <h3 class="livre-titre"><?php the_title(); ?></h3>
+
+                    <?php if ($auteur = get_field('auteur')) : ?>
+                        <p class="livre-auteur"><strong>Auteur :</strong> <?php echo esc_html($auteur); ?></p>
+                    <?php endif; ?>
+
+                    <?php if ($annee = get_field('annee')) : ?>
+                        <p class="livre-annee"><strong>Année :</strong> <?php echo esc_html($annee); ?></p>
+                    <?php endif; ?>
+
+                    <?php if ($description = get_field('description')) : ?>
+                        <p class="livre-description"><?php echo esc_html($description); ?></p>
+                    <?php endif; ?>
+
+                </div>
+
+            <?php endwhile; ?>
+        </div>
+
+        <?php wp_reset_postdata(); ?>
+
+    <?php endif;
+
+    return ob_get_clean();
+}
+add_shortcode('bibliographie', 'shortcode_bibliographie');
