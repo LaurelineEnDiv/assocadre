cfcf8cae173b581f864028014ee3073fd05a3b87
Laureline Auger <laureline.auger@gmail.com>
2025-12-02 22:50:07 +0100

    ajout documents techniques

--- functions.php
+++ functions.php
@@ -56,6 +56,78 @@ function liste_documents_shortcode() {
 add_shortcode('liste_documents', 'liste_documents_shortcode');
 
 
+// ===== Shortcode Documents Techniques ===== //
+function liste_documents_techniques_shortcode() {
+
+    ob_start();
+
+    $args = array(
+        'post_type'      => 'document_technique',
+        'posts_per_page' => -1,
+        'orderby'        => 'title',
+        'order'          => 'ASC'
+    );
+
+    $docs = new WP_Query($args);
+
+    if ( $docs->have_posts() ) : ?>
+
+        <div class="articles-grid">
+            <ul class="articles-list">
+
+                <?php while ( $docs->have_posts() ) : $docs->the_post(); ?>
+
+                    <?php 
+                        $fichier = get_field('document_technique');  
+                        $auteur = get_field('auteur');  
+                        $date_doc = get_field('date');  
+                    ?>
+
+                    <li class="item-document">
+
+                        <h3 class="document-titre">
+
+                            <?php if ($fichier) : ?>
+                                <a href="<?php echo esc_url($fichier); ?>" target="_blank">
+                                    <?php the_title(); ?>
+                                </a>
+                            <?php else : ?>
+                                <?php the_title(); ?>
+                            <?php endif; ?>
+
+                        </h3>
+
+                        <?php if ($auteur) : ?>
+                            <p class="document-auteur"><strong>Auteur :</strong> 
+                                <?php echo esc_html($auteur); ?>
+                            </p>
+                        <?php endif; ?>
+
+                        <?php if ($date_doc) : ?>
+                            <p class="document-annee"><strong>Date :</strong> 
+                                <?php echo esc_html($date_doc); ?>
+                            </p>
+                        <?php endif; ?>
+
+                    </li>
+
+                <?php endwhile; ?>
+
+            </ul>
+        </div>
+
+        <?php wp_reset_postdata();
+
+    else :
+        echo '<p>Aucun document technique disponible pour le moment.</p>';
+    endif;
+
+    return ob_get_clean();
+}
+add_shortcode('documents_techniques', 'liste_documents_techniques_shortcode');
+
+
+
 // ===== Shortcode Bibliographie ===== //
 function shortcode_bibliographie() {
 
